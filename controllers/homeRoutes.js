69339d4ca872d5f125c3c80fd8ec57d2ab8fb244
AcedYu <alexyu299@hotmail.com>
2021-05-06 11:10:25 -0700

    updated homeroutes to serve data to the views

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,39 +4,70 @@ const withAuth = require('../utils/auth');
 
 // render homepage
 router.get('/', async (req, res) => {
-  try{
-    res.render('homepage');
+  try {
+    res.render('homepage', {
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 router.get('/collection', async (req, res) => {
-  try{
-    res.render('collection');
+  try {
+    const userData = await User.findOne({
+      where: {
+        id: req.session.user_id,
+      },
+      include: [
+        {
+          model: Deck,
+          include: {
+            model: Card,
+            through: {
+              model: DeckCards,
+            },
+          },
+        },
+        {
+          model: Card,
+          through: {
+            model: UserCards,
+          }
+        },
+      ],
+    });
+    const user = userData.get({ plain: true });
+    res.render('collection', {
+      user,
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 router.get('/admin', async (req, res) => {
-  try{
+  try {
     res.render('admin');
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/auth', async (req, res) => {
-  try{
-    res.render('auth');
-  } catch (err) {
-    res.status(500).json(err);
+// Render login screen
+router.get('/auth', (req, res) => {
+  // if the session confirms that the user is logged in redirect to the homepage
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
   }
+
+  res.render('auth');
 });
 
 router.get('/confirmation', async (req, res) => {
-  try{
+  try {
     res.render('confirmation');
   } catch (err) {
     res.status(500).json(err);
@@ -44,7 +75,7 @@ router.get('/confirmation', async (req, res) => {
 });
 
 router.get('/create', async (req, res) => {
-  try{
+  try {
     res.render('create');
   } catch (err) {
     res.status(500).json(err);
@@ -52,15 +83,35 @@ router.get('/create', async (req, res) => {
 });
 
 router.get('/deck', async (req, res) => {
-  try{
-    res.render('deck');
+  try {
+    const userData = await User.findOne({
+      where: {
+        id: req.session.user_id,
+      },
+      include: [
+        {
+          model: Deck,
+          include: {
+            model: Card,
+            through: {
+              model: DeckCards,
+            },
+          },
+        },
+      ],
+    });
+    const user = userData.get({ plain: true });
+    res.render('deck', {
+      user,
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 router.get('/play', async (req, res) => {
-  try{
+  try {
     res.render('play');
   } catch (err) {
     res.status(500).json(err);
@@ -68,21 +119,20 @@ router.get('/play', async (req, res) => {
 });
 
 router.get('/store', async (req, res) => {
-  try{
-    res.render('store');
+  try {
+    const factionData = await Faction.findAll({
+      include: {
+        model: Card,
+      },
+    });
+    const factions = factionData.map(faction => faction.get({plain: true}));
+    res.render('store', {
+      factions,
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
-// Login
-// router.get('/login', (req, res) => {
-//   if (req.session.logged_in) {
-//     res.redirect('/');
-//     return;
-//   }
-
-//   res.render('login');
-// });
 
 module.exports = router;
