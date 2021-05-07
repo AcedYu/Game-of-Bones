8badcb8e790c919ccee80b2285fe58460ced75f6
Manuel Villasenor <manuelvillasenor@2@gamil.com>
2021-05-07 10:35:37 -0700

    collection/game bugs

--- public/js/deckViewCreate.js
+++ public/js/deckViewCreate.js
@@ -1,6 +1,8 @@
 var cardTemplateArea = $('#unknown')
 
 
+var collectionArea = $('#collection')
+
 const viewCardCollection = async (event) => {
 
   var currentPlayerId = 1
@@ -21,6 +23,25 @@ const viewCardCollection = async (event) => {
   
   for (var i = 0 ; i<res.cards.length;i++){
 
+    let cardDiv = $('<div>');
+            
+    collectionArea.append(cardDiv)
+            
+                let cardTemplate = $(`
+                <div id="card-1" class="card card-play">
+                    <img id="card-1-img" src="${res.cards[i].image_url}" class="card-img-top" alt="photo">
+                    <div id="card-1-body" class="card-body">
+                    <h5 id="card-1-title" class="card-title">${res.cards[i].name}</h5>
+                    </div>
+                    <ul id="card-1-list" class="">
+                    <li id="card-1-attack" class="list-item"><i class="fad fa-swords me-2"></i>${res.cards[i].attack}</li>
+                    <li id="card-1-faction"  class="list-item"><i class="fad fa-eye-evil me-2"></i>${res.cards[i].faction.name}</li>
+                    </ul>
+                    <div id="card-1-footer" class="card-footer">
+                    <small id="card-1-id" >${res.cards[i].id}</small>
+                    </div>
+                </div>`)
+                cardDiv.append(cardTemplate)
   }
 
 
